a866366ac18fefb296f7bcc8afb8bc0428275702
yuraigle <yuraigle@gmail.com>
2025-06-22 13:25:02 +0800

    .

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ReportVdiView from '@/views/ReportVdiView.vue'
 import ReportVdaView from '@/views/ReportVdaView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,10 +9,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      // route level code-splitting
-      // this generates a separate chunk (Home.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/HomeView.vue'),
+      component: HomeView,
     },
     {
       path: '/vdi',
@@ -22,12 +20,7 @@ const router = createRouter({
       path: '/vda',
       name: 'vda',
       component: ReportVdaView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
-    },
+    }
   ],
 })
 
--- src/views/AboutView.vue
+++
@@ -1,5 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div class="container-fluid">О программе</div>
-</template>

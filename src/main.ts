55479a8accb98543df122021814e688d495920e5
yuraigle <yuraigle@gmail.com>
2025-06-22 20:31:32 +0800

    .

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import 'bootswatch/dist/cosmo/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -7,27 +7,22 @@ import LoginView from '@/views/LoginView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView,
-    },
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/vdi',
-      name: 'vdi',
-      component: ReportVdiView,
-    },
-    {
-      path: '/vda',
-      name: 'vda',
-      component: ReportVdaView,
-    }
+    { path: '/', name: 'home', component: HomeView },
+    { path: '/login', name: 'login', component: LoginView },
+    { path: '/vdi', name: 'vdi', component: ReportVdiView },
+    { path: '/vda', name: 'vda', component: ReportVdaView },
   ],
 })
+
+router.beforeEach(async (to) => {
+  const publicPages = ['/login'];
+  const authRequired = !publicPages.includes(to.path);
+  // const auth = useAuthStore();
+
+  if (authRequired) {
+    // auth.returnUrl = to.fullPath;
+    return '/login';
+  }
+});
 
 export default router

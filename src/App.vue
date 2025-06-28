7245267bfc501d1f8c6cec7dccf8182a0c41e02a
yuraigle <yuraigle@gmail.com>
2025-06-28 12:03:07 +0800

    app footer

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,26 @@
 import { RouterView } from 'vue-router'
 import AppNavbar from '@/components/AppNavbar.vue'
 import ToastsContainer from '@/components/ToastsContainer.vue';
+import AppFooter from './components/AppFooter.vue';
 </script>
 
 <template>
   <AppNavbar />
   <ToastsContainer />
   <RouterView />
+  <AppFooter />
 </template>
+
+<style>
+/* layout with footer on the bottom of the page */
+html,
+body,
+#app {
+  height: 100% !important;
+}
+
+#app {
+  display: flex !important;
+  flex-direction: column !important;
+}
+</style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { COMPANY_NAME } from '@/../environment';
+const version = APP_VERSION;
+</script>
+
+<template>
+  <footer class="mt-auto">
+    <div class="d-flex p-2 mt-4 small">
+      <div class="flex-grow-1">
+        &copy; {{ new Date().getFullYear() }} {{ COMPANY_NAME }}
+      </div>
+      <div class="flex-shrink-1 text-end">
+        v{{ version }}
+      </div>
+    </div>
+  </footer>
+</template>
